fbd8f920ed836a722b5a43f95e6ae9c7e915f3d1
jianghao <jianghao@haplox.com>
2025-05-26 08:56:09 +0800

    fix: 更新主题和gitlens

--- lua/community.lua
+++ lua/community.lua
@@ -5,12 +5,14 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" }
+  -- { import = "astrocommunity.colorscheme.onedark" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua" },
   -- { import = "astrocommunity.completion.dracula" },
   -- example of importing an entire language pack
   -- these packs can set up things such as Treesitter, Language Servers, additional language specific plugins, and more!
-  { import = "astrocommunity.colorscheme.tokyodark-nvim" }
+  -- { import = "astrocommunity.colorscheme.tokyodark-nvim" }
   -- { import = "astrocommunity.colorscheme.aurora" }
   -- { import = "astrocommunity.colorscheme.bamboo-nvim" }
 }
